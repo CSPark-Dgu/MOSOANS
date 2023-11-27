5ab3092ed4338982fd2a798abdcd7e2e95a429e3
Changsu Park <gd12091@gmail.com>
2023-11-27 22:18:26 +0900

    first app

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -1,28 +1,33 @@
 package com.example.assignment;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {   //콜백 함수
 
+    public static final String EXTRA_MESSAGE = "com.example.assignment.MESSAGE"; //메시지를 전달하기 위한 키 값
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState); //기본 화면 구성
+        setContentView(R.layout.activity_main); //추가 화면 구현
     }
 
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.d("MainActivity", "onPause 호출됨");
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextText);
+        String message = editText.getText().toString(); //입력한 문자열을 가져옴
+        intent.putExtra(EXTRA_MESSAGE, message); //intent에 message를 넣음
+        startActivity(intent); //intent를 실행
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("MainActivity", "onStart 호출됨");
-    }
 
 
 }
