a8877b9bf0148fd23ebb0ea4308e3b0111469473
Changsu Park <gd12091@gmail.com>
2023-11-29 18:44:21 +0900

    Second app

--- Assignement2/app/build.gradle.kts
+++ Assignement2/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.mysecondapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mysecondapp"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- Assignement2/app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ Assignement2/app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.mysecondapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    String message = "Android App Message";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(message, "onCreate() operated!!");
+
+        text = (TextView)findViewById(R.id.textView);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(message, "onResume() operated!!");
+        text = (TextView)findViewById(R.id.textView);
+        text.setText("My onResume()!!");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(message, "onPause() operated!!");
+        text = (TextView)findViewById(R.id.textView);
+        text.setText("My onPause()!!");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(message, "onStop() operated!!");
+        text = (TextView)findViewById(R.id.textView);
+        text.setText("My onStop()!!");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(message, "onDestroy() operated!!");
+        text = (TextView)findViewById(R.id.textView);
+        text.setText("My onDestroy()!!");
     }
 }
